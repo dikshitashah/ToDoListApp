25de17fc7a51c9cef66a9cf7f57da08e819a00cd
Dikshita Shah <dikshitashah0810@gmail.com>
2023-03-07 17:13:55 +0530

    Sample commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is a ToDo List App
+        <p>This will help us to keep our tasks planned for the day and delete them when they are done. This will increase productivity and would help us in planning our goals</p>
+    </div>
+  )
+}
